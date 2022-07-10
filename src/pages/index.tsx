655accb838692b71f61741e9d4f30a365f450f98
Pablo Alan <pabloxt14@gmail.com>
2022-07-10 16:02:31 -0300

    feat: creating global config from stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
+import { Wrapper } from '../components/Wrapper';
 
-export default function Home() {
+export default function Home(props) {
+  const { title, description, image } = props;
+
   return (
-    <div>
-      Hello World
-    </div>
+    <Wrapper>
+      <Head>
+        <title>{title}</title>
+        <meta content={title} property="og:title" />
+        <meta content={description} name="descriprion" />
+        <meta content={description} property="og:descriprion" />
+        <meta content="https://pabloalan.vercel" property="og:url" />
+      </Head>
+
+      Content
+    </Wrapper>
   )
 }
